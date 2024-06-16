f70b9ebaea2ed81c700f33054b6198e6f03ee649
Fachry Haswanto <fachry111@gmail.com>
2024-06-17 02:42:50 +0700

    fixing jwt key

--- src/main/java/com/pajakbpkad/pajakbpkad/utils/JWTUtils.java
+++ src/main/java/com/pajakbpkad/pajakbpkad/utils/JWTUtils.java
@@ -9,27 +9,40 @@ import java.util.function.Function;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
 
 @Component
 public class JWTUtils {
     
     private SecretKey Key;
     private static final long EXPIRATION_TIME = 18000000; //5 hours by milisecond 18000000
+
+    @Value("${jwt.key}")
+    private String secretString;
     
     public JWTUtils() {
-
-        String secretString = "%JLH#UE7<`Gtf_Z~uax'%sL/h]XA2=Fw";
 
         byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
 
         this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
     } 
+
+    // @PostConstruct
+    // public void init() {
+    //     try {
+    //         byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+    //         this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    //     } catch (IllegalArgumentException e) {
+    //         throw new RuntimeException("Invalid Base64 encoded secret key", e);
+    //     }
+    // }
     
     public String generateToken(UserDetails userDetails){
         return Jwts.builder()
